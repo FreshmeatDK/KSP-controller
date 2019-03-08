926a7df45372d449552acb5432361ba68b93331f
Jesper Krogh Petersen <jkroghp@hotmail.com>
2019-03-08 22:26:01 +0100

    Flameout check first try

--- Controller/Client/Client.py
+++ Controller/Client/Client.py
@@ -31,16 +31,26 @@ def main_loop():
     ctrl[1] = 0
     mainParts = listMainParts(vessel) #some actions only in parts not connected by docking ports
     time.sleep(1)
-    updateTime = time.time()
-    initTime = time.time()
+    updateTime = time.perf_counter()
+    initTime = time.perf_counter()
+    engineCheckTime = time.perf_counter()
     print('init')
+    flameOut = False
 
     try:
       while vessel == conn.space_center.active_vessel:
+          now = time.perf_counter()
+          errorcode = 1          # 0 = success, 1 = unspec/timeout, 2 = overflow
+          
+          if (now - engineCheckTime > 1): #check for flameout every second.engines
+            decoupleList = vessel.parts.in_decouple_stage(vessel.control.current_stage-1)
+            engineList = vessel.parts.engines
+            flameOut = False
+            for Engine in engineList:
+                if not (Engine.has_fuel):
+                    flameOut = True
+                    print('Flameout')
 
-          thisTime = time.time()
-          errorcode = 1
-          # 0 = success, 1 = unspec/timeout, 2 = overflow
           if arduino.in_waiting > 0:
             inData=struct.unpack('<B',arduino.read())
             ctrlByteNum = 0
@@ -48,38 +58,38 @@ def main_loop():
                 arduino.write(0b10101010) #send wait to arduino
                 print("overflow: ",arduino.in_waiting)
                 conn.ui.message("Overflow", position = conn.ui.MessagePosition.top_left)
-                arduino.flushInput()
-            elif (thisTime - updateTime) > 0.1:
-                  updateTime = time.time()
-                  #conn.ui.message("Ack", position = conn.ui.MessagePosition.top_left)
+                arduino.reset_input_buffer()
+            elif (now - updateTime) > 0.1:
+                  updateTime = time.perf_counter()
+                  conn.ui.message("Ack", position = conn.ui.MessagePosition.top_left)
                   arduino.write(0b01010101)
  
             for i in range(2):
                 oldCtrl[i] = ctrl[i]
 
-            if inData[0] == 0b10101010:
+            if inData[0] == 0b10101010: #start char
           
                 readOp = True
                 waitloop = 1
                 while readOp:
                     waitloop += 1
-                    if waitloop > 1000:
-                        readOp = False
+                    if waitloop > 1000: #timeout
+                        readOp = False   #leave
                     if arduino.in_waiting > 0:
                         inData=struct.unpack('<B',arduino.read())
-                    if inData[0] == 0b11001100:
+                    if inData[0] == 0b11001100: #EOF char
                         readOp = False
-                        if (ctrlByteNum == 2 and errorcode == 1):
+                        if (ctrlByteNum == 2 and errorcode == 1): #if we have reached two bytes
                             errorcode = 0
 
-                    elif inData[0] == 0b00001111:
-                        inData=struct.unpack('<B',arduino.read())
-                        ctrl[ctrlByteNum] = inData[0]
+                    elif inData[0] == 0b00001111: #escape char
+                        inData=struct.unpack('<B',arduino.read())     #read another
+                        ctrl[ctrlByteNum] = inData[0]                 #and copy directly
                         ctrlByteNum += 1
                         if ctrlByteNum > 1:
                             ctrlByteNum = 1
                             errorcode = 2
-                    else: 
+                    else:                               #not esc, not EOF
                         if ctrlByteNum > 1:
                             ctrlByteNum = 1
                             errorcode = 2
@@ -89,14 +99,14 @@ def main_loop():
 
             if errorcode == 0: # we have success, run commands
 
-                if ((ctrl[0] != oldCtrl[0]) or (ctrl[1] != oldCtrl[1]) and thisTime-initTime > 1 ):
+                if ((ctrl[0] != oldCtrl[0]) or (ctrl[1] != oldCtrl[1]) and now-initTime > 1 ):
                     actions(ctrl,oldCtrl,vessel, mainParts)
                     camera = (ctrl[0]&0b11100000)>>5
                     camcontrol(camera, cam)
 
-          elif (thisTime - updateTime) > 1:
-                  updateTime = time.time()
-                  conn.ui.message("Return flow", position = conn.ui.MessagePosition.top_left)
+          elif (now - updateTime) > 1: #more than one second since last received comms from arduino
+                  updateTime = time.perf_counter()
+                  conn.ui.message("Arduino timeout", position = conn.ui.MessagePosition.top_left)
                   arduino.write(0b01010101)
 
 
